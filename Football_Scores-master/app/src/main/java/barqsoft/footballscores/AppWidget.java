483588845be6dd2454d7cbe83c4fcd1867e6d0ae
padlanau <padlanau@yahoo.com.au>
2015-10-05 18:07:22 +1000

    Football Scores

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/AppWidget.java
@@ -0,0 +1,90 @@
+/***
+  Copyright (c) 2008-2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+*/
+
+package barqsoft.footballscores;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import barqsoft.footballscores.service.myFetchService;
+
+public class AppWidget extends AppWidgetProvider {
+
+  
+  @Override
+  public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
+    ComponentName me=new ComponentName(ctxt, AppWidget.class);
+      
+    mgr.updateAppWidget(me, buildUpdate(ctxt, appWidgetIds));
+  }
+  
+  private RemoteViews buildUpdate(Context ctxt, int[] appWidgetIds) {
+    RemoteViews updateViews=new RemoteViews(ctxt.getPackageName(), R.layout.widget);
+  
+    Intent i=new Intent(ctxt, AppWidget.class);
+    
+    i.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+    i.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+    
+    PendingIntent pi= PendingIntent.getBroadcast(ctxt, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+      //
+      int mPages = PagerFragment.NUM_PAGES;
+
+      Intent service_start = new Intent(ctxt, myFetchService.class);
+      ctxt.startService(service_start);
+
+      SimpleDateFormat match_date = new SimpleDateFormat("yyyy-MM-dd");
+
+    // today
+      Date today = Calendar.getInstance().getTime();
+      String mToday = match_date.format(today);
+      String[] argsToday = { mToday};
+
+      // tomorrow
+      Calendar cal = Calendar.getInstance();
+      cal.add(Calendar.DAY_OF_YEAR, 1);
+      Date tomorrow = cal.getTime();
+      String mTomorrow = match_date.format(tomorrow);
+      String[] argsTomorrow = {mTomorrow};
+
+      // Get Today's number of games
+      Uri uri = Uri.withAppendedPath(DatabaseContract.BASE_CONTENT_URI, "date");    // content://barqsoft.footballscores/date
+      Cursor cursor = ctxt.getContentResolver().query(uri, null, null, argsToday, null);
+      int mCountResults = cursor.getCount();
+
+      updateViews.setTextViewText(R.id.textview_today,  ctxt.getString(R.string.today_gameresults) + String.valueOf(mCountResults));
+      updateViews.setContentDescription(R.id.textview_today, ctxt.getString(R.string.today_gameresults) + String.valueOf(mCountResults));
+
+    // Get Tomorrow's number of games
+      uri = Uri.withAppendedPath(DatabaseContract.BASE_CONTENT_URI, "date");    // content://barqsoft.footballscores/date
+      cursor = ctxt.getContentResolver().query(uri, null, null, argsTomorrow, null);
+      mCountResults = cursor.getCount();
+
+      updateViews.setTextViewText(R.id.textview_tomorrow,  ctxt.getString(R.string.tomorrow_gameresults) + String.valueOf(mCountResults));
+      updateViews.setContentDescription(R.id.textview_tomorrow, ctxt.getString(R.string.tomorrow_gameresults) + String.valueOf(mCountResults));
+    return(updateViews);
+  }
+}
